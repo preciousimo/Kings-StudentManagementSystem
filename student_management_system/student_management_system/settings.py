2b4709aa34d117d8c0722f7b6d8fe09c71d48168
John Solomon <jolomonson@gmail.com>
2021-12-29 20:41:54 +0100

    Setting database Config

--- student_management_system/student_management_system/settings.py
+++ student_management_system/student_management_system/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'student_management_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'student_management_system',   
+        'USER': 'student_management_system',
+        'PASSWORD': 'student_management_password',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
