7ff8381c68d67b2a6c2435bc2010e6d6d7f8d49b
JolomonSon <johnsolo2002@gmail.com>
2022-01-02 22:05:48 +0100

    Student model created

--- student_management_system/student_management_app/models.py
+++ student_management_system/student_management_app/models.py
@@ -23,3 +23,18 @@ class Subjects(models.Model):
     id = models.AutoField(primary_key=True)
     subect_name = models.CharField(max_length=255)
     course_id = models.ForeignKey(Courses, delete=models.CASCADE)
+
+class Students(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    gender = models.CharField(max_length=255)
+    profile_picture = models.FileField(max_length=255)
+    address = models.TextField()
+    state = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
