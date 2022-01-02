bd686571abda9ca2d0f3fbf9bca75798a3ea8f85
JolomonSon <johnsolo2002@gmail.com>
2022-01-02 21:58:49 +0100

    Subject model created(with FK)

--- student_management_system/student_management_app/models.py
+++ student_management_system/student_management_app/models.py
@@ -18,4 +18,8 @@ class Courses(models.Model):
     course_name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
-    
+
+class Subjects(models.Model):
+    id = models.AutoField(primary_key=True)
+    subect_name = models.CharField(max_length=255)
+    course_id = models.ForeignKey(Courses, delete=models.CASCADE)
