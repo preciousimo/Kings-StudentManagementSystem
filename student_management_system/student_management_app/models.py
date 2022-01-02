45ec9f52cc6bc6d9c4e03e4cd72003985a8002fe
JolomonSon <johnsolo2002@gmail.com>
2022-01-02 21:33:30 +0100

    Admin model created

--- student_management_system/student_management_app/models.py
+++ student_management_system/student_management_app/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Admin(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objectsmodels.Manager()
