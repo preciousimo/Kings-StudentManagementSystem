806ae9e9d9eb8236cf7d0e3de63bcd66125710d5
JolomonSon <johnsolo2002@gmail.com>
2022-01-03 18:17:04 +0100

    Updated FeedBack models

--- student_management_system/student_management_app/models.py
+++ student_management_system/student_management_app/models.py
@@ -96,19 +96,19 @@ class LeaveReportStaff(models.Model):
     updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
 
-class FeedBackStudent(models.Model):
+class FeedBackStudents(models.Model):
     id = models.AutoField(auto_now_add=True)
     student_id = models.ForeignKey(Students, on_delete=models.CASCADE)
-    feedback = models.CharField(max_length=255)
+    feedback = models.TextField()
     feedback_reply = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
 
-class FeedBackStudent(models.Model):
+class FeedBackStaffs(models.Model):
     id = models.AutoField(auto_now_add=True)
     staff_id = models.ForeignKey(Staffs, on_delete=models.CASCADE)
-    feedback = models.CharField(max_length=255)
+    feedback = models.TextField()
     feedback_reply = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
