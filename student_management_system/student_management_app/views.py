745da0865550ab94ced840a52d02a2a26f8d2954
Mary Solomon <johnsolo2002@gmail.com>
2022-01-10 11:13:31 +0100

    Added plugins to static folder and created media folder

--- student_management_system/student_management_app/views.py
+++ student_management_system/student_management_app/views.py
@@ -12,20 +12,3 @@ def sign_in(request):
 
 
 def sign_out(request):
-
-=======
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render
-
-# Create your views here.
-def home(request):
-    
-
-def sign_in(request):
-
-
-def sign_out(request):
-
->>>>>>> 2a4ef639d93a5c656da9029f7c4c85e72d101b23
