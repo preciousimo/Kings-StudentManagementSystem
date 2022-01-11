18136b528f6e9c51abecfe4ea5b1739b6c86f7c0
Mary Solomon <johnsolo2002@gmail.com>
2022-01-11 16:06:40 +0100

    urls and views updated for home page

--- student_management_system/student_management_app/views.py
+++ student_management_system/student_management_app/views.py
@@ -5,10 +5,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
+def homePage(request):
     
+    return render (request, 'home.html')
 
-def sign_in(request):
-
-
-def sign_out(request):
--- student_management_system/student_management_system/urls.py
+++ student_management_system/student_management_system/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf.urls.static import static
+
+from student_management_app import views
+from student_management_system import settings
+
 from django.contrib import admin
 
 urlpatterns = [
+    path('home', view.homePage),
     url(r'^admin/', admin.site.urls),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
