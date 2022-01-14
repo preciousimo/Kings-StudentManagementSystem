82a945dcd9de168c2c53c1ce9472c06e8c5333ed
Mary Solomon <johnsolo2002@gmail.com>
2022-01-14 14:35:22 +0100

    Handling auth error

--- student_management_system/student_management_app/models.py
+++ student_management_system/student_management_app/models.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+#from django.db.models.signals import post_save
+#from django.dispatch import receiver
 # Create your models here.
 
 class CustomUser(AbstractUser):
     user_type_data = ((1, "HOD"), (2, "Staff"), (3, "Student"))
     user_type = models.CharField(default = 1, choices = user_type_data, max_length=10)
 
-class Admin(models.Model):
+class AdminHOD(models.Model):
     id = models.AutoField(primary_key=True)
     admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
@@ -62,14 +64,14 @@ class Students(models.Model):
     country = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
-    course_id = models.ForeignKey(Courses, on_delete=models.CASCADE)
+    course_id = models.ForeignKey(Courses, on_delete=models.DO_NOTHING)
     session_start_year = models.DateField()
     session_end_year = models.DateField()
     objects = models.Manager()
 
 class Attendance(models.Model):
     id = models.AutoField(primary_key=True)
-    subject_id = models.ForeignKey(Subjects, on_delete=models.CASCADE)
+    subject_id = models.ForeignKey(Subjects, on_delete=models.DO_NOTHING)
     attendance_date = models.DateTimeField(auto_now_add=True)
     attendance_time = models.DateTimeField(auto_now_add=True)
     created_at = models.DateTimeField(auto_now_add=True)
