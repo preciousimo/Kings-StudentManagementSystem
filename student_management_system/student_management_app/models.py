6ef831b746d881a63388e49b8dc181a161c43cd8
Mary Solomon <johnsolo2002@gmail.com>
2022-01-16 21:33:23 +0100

    Removed Conflicting Model, Renamed Existing Model

--- student_management_system/student_management_app/models.py
+++ student_management_system/student_management_app/models.py
@@ -1,18 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 #from django.db.models.signals import post_save
 #from django.dispatch import receiver
 # Create your models here.
 
-class CustomUser(AbstractUser):
-    user_type_data = ((1, "HOD"), (2, "Staff"), (3, "Student"))
-    user_type = models.CharField(default = 1, choices = user_type_data, max_length=10)
-
-class AdminHOD(models.Model):
+class Admin(models.Model):
     id = models.AutoField(primary_key=True)
-    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
@@ -29,7 +23,6 @@ class Courses(models.Model):
 
 class Staffs(models.Model):
     id = models.AutoField(primary_key=True)
-    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
@@ -52,7 +45,6 @@ class Subjects(models.Model):
 
 class Students(models.Model):
     id = models.AutoField(primary_key=True)
-    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
