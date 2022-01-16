f3869772064aa3fb82ebb9eb39cb87ae53dc47ed
Mary Solomon <johnsolo2002@gmail.com>
2022-01-16 21:48:12 +0100

    Reverted changes and migrated successfully

--- student_management_system/student_management_app/models.py
+++ student_management_system/student_management_app/models.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 #from django.db.models.signals import post_save
 #from django.dispatch import receiver
 # Create your models here.
 
+class CustomUser(AbstractUser):
+    user_type_data = ((1, "HOD"), (2, "Staff"), (3, "Student"))
+    user_type = models.CharField(default = 1, choices = user_type_data, max_length=10)
+
 class AdminHOD(models.Model):
     id = models.AutoField(primary_key=True)
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
@@ -23,6 +29,7 @@ class Courses(models.Model):
 
 class Staffs(models.Model):
     id = models.AutoField(primary_key=True)
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
@@ -45,6 +52,7 @@ class Subjects(models.Model):
 
 class Students(models.Model):
     id = models.AutoField(primary_key=True)
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
