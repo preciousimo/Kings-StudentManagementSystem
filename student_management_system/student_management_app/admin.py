32961f52ab214f2d24ed320c13c62e5341895698
Mary Solomon <johnsolo2002@gmail.com>
2022-01-17 22:32:14 +0100

    Blank UserModel created for password encryption

--- student_management_system/student_management_app/admin.py
+++ student_management_system/student_management_app/admin.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
-
+from student_management_app.models import CustomUser
 # Register your models here.
+class UserModel(UserAdmin):
+    pass
+
+admin.site.register(CustomUser, UserModel)
