8d5942024b5ccbf5407b8741752f5c08de66328a
Mary Solomon <johnsolo2002@gmail.com>
2022-01-17 16:42:01 +0100

    Added CustomUser and migrated successfully

--- student_management_system/student_management_system/settings.py
+++ student_management_system/student_management_system/settings.py
@@ -131,4 +131,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-#AUTH_USER_MODEL = "student_management_app.CustomUser"
+AUTH_USER_MODEL = "student_management_app.CustomUser"
