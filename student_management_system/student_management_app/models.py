56c2173d33814bd7ef6d7e0457a0409f72b864a9
Mary Solomon <johnsolo2002@gmail.com>
2022-01-17 23:22:50 +0100

    Implemented Signaling for new CustomUser

--- student_management_system/student_management_app/models.py
+++ student_management_system/student_management_app/models.py
@@ -1,8 +1,8 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-#from django.db.models.signals import post_save
-#from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 class CustomUser(AbstractUser):
@@ -126,9 +126,26 @@ class NotificationStudents(models.Model):
 class NotificationStaffs(models.Model):
     id = models.AutoField(primary_key=True)
     staff_id = models.ForeignKey(Staffs, on_delete=models.CASCADE)
-    message = -models.TextField()
+    message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
 
 @receiver(post_save, sender=CustomUser)
+def create_user_profile(sender,instance,created,**kwargs):
+    if created:
+        if instance.user_type==1:
+            AdminHOD.objects.create(admin=instance)
+        if instance.user_type==2:
+            Staffs.objects.create(admin=instance)
+        if instance.user_type==3:
+            Students.objects.create(admin=instance)
+
+@receiver(post_save,sender=CustomUser)
+def create_user_profile(sender,instance,**kwargs):
+    if instance.user_type==1:
+        instance.adminhod.save()
+    if instance.user_type==2:
+        instance.staffssave()
+    if instance.user_type==3:
+        instance.students.save()
