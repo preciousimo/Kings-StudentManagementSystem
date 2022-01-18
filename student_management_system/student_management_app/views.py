ef9af5f019b3d801e46369b55467cbff75c1158f
Mary Solomon <johnsolo2002@gmail.com>
2022-01-18 15:10:56 +0100

    Implemented login auth successfully

--- student_management_system/student_management_app/views.py
+++ student_management_system/student_management_app/views.py
@@ -2,6 +2,8 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from student_management_app.EmailBackEnd import EmailBackEnd
+
 # Create your views here.
 def homePage(request):
     
@@ -14,4 +16,9 @@ def login_user(request):
     if request.method != "POST":
         return HttpResponse("<h2>Method not allowed</h2>")
     else:
-        return HttpResponse("Email :"+request.POST.get('email')+ " Password:"+request.POST.get('password'))
+        user = EmailBackEnd.authenticate(request,request.POST.get('email'),request.POST.get('password'))
+        if user != None:
+            login(user)
+            return HttpResponse("Email :"+request.POST.get('email')+ " Password:"+request.POST.get('password'))
+        else: 
+            return HttpResponse('Invalid Login')
