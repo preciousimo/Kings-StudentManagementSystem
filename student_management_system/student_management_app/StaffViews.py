759a2218f37f8e4953a0b51eae388127c6a5d9f8
Mary Solomon <johnsolo2002@gmail.com>
2022-01-21 11:48:44 +0100

    creaeted function to save staff information

--- student_management_system/student_management_app/StaffViews.py
+++ student_management_system/student_management_app/StaffViews.py
@@ -1,8 +1,20 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-def staff_home(request):
+from django.contrib import messages
 
+
+def staff_home(request):
     return render(request, 'staff_templates/includes/home_content.html')
 
 def add_staff(request):
-
     return render(request, 'staff_templates/add_staff_template.html')
+
+def add_staff_save(request):
+    if request.method != 'POST':
+        return HttpResponse('<h2>Method not allowed</h2>')
+        return HttpResponseRedirect('staff_templates/add_staff_template.html')
+    else:
+        if request.POST.get == None:
+            messages.error('Invalid Input')
+        else:
+            firstname = request.POST.get('firstname')
