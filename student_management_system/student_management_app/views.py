1fb68834c6816c306a7428068bdbe005bb1eec8f
Mary Solomon <johnsolo2002@gmail.com>
2022-01-22 12:15:09 +0100

    Removed fields in staff model and dropped table for registration testing

--- student_management_system/student_management_app/views.py
+++ student_management_system/student_management_app/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.contrib import messages
 from student_management_app.EmailBackEnd import EmailBackEnd
 from student_management_app import HodViews
+from student_management_app.models import CustomUser
 
 # Create your views here.
 def homePage(request):
@@ -17,8 +18,20 @@ def registerPage(request):
     return render (request, 'register.html')
 
 def registerUser(request):
-    return HttpResponseRedirect('admin_home')
-
+    if request.method != 'POST':
+        return HttpResponse('<h2>Method not allowed</h2>')
+        return HttpResponseRedirect('register')
+    else:
+        first_name = request.POST.get('first_name')
+        second_name = request.POST.get('second_name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        user = CustomUser.objects.create_user(first_name=first_name,second_name=second_name,email=email,password=password,password2=password2,user_type=2)
+        if user is True:
+            user.save()
+        else:
+            return HttpResponse('Registration Successful')
 
 def logoutUser(request):
     logout(request, user)
