590187caa8b6019eae09375d9a42c3cd9902f1fe
Mary Solomon <johnsolo2002@gmail.com>
2022-01-28 17:14:49 +0100

    Added other objects to Student class

--- student_management_system/student_management_app/admin.py
+++ student_management_system/student_management_app/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Students, Staffs, Courses
+from .models import Students, Staffs
 # Register your models here.
 
 admin.site.register(Students)
 admin.site.register(Staffs)
-admin.site.register(Courses)
