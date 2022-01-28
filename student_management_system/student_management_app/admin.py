b33a1751854748c7ee26ab2c5c771b9e3411ffa3
Mary Solomon <johnsolo2002@gmail.com>
2022-01-28 16:44:23 +0100

    created second superuser and registered 3 models in admin

--- student_management_system/student_management_app/admin.py
+++ student_management_system/student_management_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Students, Staffs, Courses
 # Register your models here.
+
+admin.site.register(Students)
+admin.site.register(Staffs)
+admin.site.register(Courses)
