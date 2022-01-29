cfa1fe54149bc5c01c94cda424448fe41717f85c
Mary Solomon <johnsolo2002@gmail.com>
2022-01-29 06:21:07 +0100

    Implemented messages in views

--- student_management_system/accounts/views.py
+++ student_management_system/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User, auth
-from student_management_app import EmailBackEnd
+from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 def loginPage(request):
@@ -17,16 +17,16 @@ def registerPage(request):
         password2 = request.POST['password2']
         if password == password2:
             if User.objects.filter(username=username).exists():
-                return HttpResponse('Username taken')
+                messages.info(request, '{{username}} already taken')
             elif User.objects.filter(email=email).exists():
-                return HttpResponse('Email taken')
+                messages.info(request, '{{email}} already taken')
             else:
                 user = User.objects.create_user(first_name=first_name,last_name=last_name,username=username,email=email,password=password)
                 user.save() 
-                return HttpResponse(username,' created successfully')   
+                messages.success(request, '{{username}} created successfully')   
                 return HttpResponseRedirect('student_management_app/admin-home')  
         else:
-            return HttpResponse('Passwords do not match')
+            messages.info(request,'Passwords do not match')
             #messages.success(request, first_name,' created successfully')  
             return render(request, 'register.html')
     else:
