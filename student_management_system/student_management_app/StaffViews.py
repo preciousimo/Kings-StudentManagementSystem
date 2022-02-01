cf8bf306bec699750c861216ebcbdcf2465decef
Mary Solomon <johnsolo2002@gmail.com>
2022-02-01 16:21:03 +0100

    Included navbars in staff page

--- student_management_system/student_management_app/StaffViews.py
+++ student_management_system/student_management_app/StaffViews.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.contrib import messages
 
 def staffHome(request):
-    return render(request, 'staff_templates/includes/home_content.html')
+    return render(request, 'staff_templates/home_content.html')
 
 def addStaff(request):
     return render(request, 'staff_templates/add_staff_template.html')
