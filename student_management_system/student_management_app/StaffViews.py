f4decbc3c9131e077282e23856eacc1a01c5052d
Mary Solomon <johnsolo2002@gmail.com>
2022-02-01 20:57:56 +0100

    HttpResponse working in add-staff-template

--- student_management_system/student_management_app/StaffViews.py
+++ student_management_system/student_management_app/StaffViews.py
@@ -1,9 +1,12 @@
-from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib import messages
+from django.http import HttpResponse
 
 def staffHome(request):
     return render(request, 'staff_templates/home_content.html')
 
 def addStaff(request):
-    return render(request, 'staff_templates/add_staff_template.html')
+    if request.method == 'POST':
+        return HttpResponse('Posting in progress')
+    else:
+        return render(request, 'staff_templates/add_staff_template.html')
