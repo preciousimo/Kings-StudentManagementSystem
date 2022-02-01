cd042ddcbb07312dd7942e87cf2c6b0ec7163d99
Mary Solomon <johnsolo2002@gmail.com>
2022-02-01 21:40:05 +0100

    Fetched staff data

--- student_management_system/student_management_app/StaffViews.py
+++ student_management_system/student_management_app/StaffViews.py
@@ -7,6 +7,18 @@ def staffHome(request):
 
 def addStaff(request):
     if request.method == 'POST':
-        return HttpResponse('Posting in progress')
+        first_name = request.POST['first_name']
+        middle_name = request.POST['middle_name']
+        last_name = request.POST['last_name']
+        date_of_birth = request.POST['date_of_birth']
+        email = request.POST['email']
+        phone_number = request.POST['phone_number']
+        gender = request.POST['gender']
+        #profile_picture = request.POST['profile_picture']
+        #curriculum_vitae = request.POST['curriculum_vitae']
+        address = request.POST['address']
+        state = request.POST['state']
+        nationality = request.POST['nationality']
+        return HttpResponse(first_name)
     else:
         return render(request, 'staff_templates/add_staff_template.html')
