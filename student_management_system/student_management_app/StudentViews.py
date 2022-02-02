2b23293cc6fe77e99115d2112661a024638d583c
Mary Solomon <johnsolo2002@gmail.com>
2022-02-02 17:51:36 +0100

    software-reuse (StaffViews.py) and modified (StudentViews)

--- student_management_system/student_management_app/StudentViews.py
+++ student_management_system/student_management_app/StudentViews.py
@@ -2,4 +2,27 @@ from django.shortcuts import render
 def studentHome(request):
     return render(request, 'student_templates/base.html')
 def addStudent(request):
-    return render(request, 'student_templates/add_student_template.html')
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        middle_name = request.POST['middle_name']
+        last_name = request.POST['last_name']
+        date_of_birth = request.POST['date_of_birth']
+        email = request.POST['email']
+        phone_number = request.POST['phone_number']
+        gender = request.POST['gender']
+        #profile_picture = request.POST['profile_picture']
+        address = request.POST['address']
+        state = request.POST['state']
+        nationality = request.POST['nationality']
+        try:
+            new_student = Students.objects.create(first_name=first_name,middle_name=middle_name,last_name=last_name,date_of_birth=date_of_birth,email=email,phone_number=phone_number,gender=gender,address=address,state=state,nationality=nationality)
+            new_student.save()
+            messages.success(request, '{} added successfully'.format(first_name))
+            return render(request, 'staff_templates/add_student_template.html')   
+        except:
+            messages.error(request, 'Failed to add new student')
+            return render(request, 'student_templates/add_student_template.html')    
+    else:
+        return render(request, 'student_templates/add_student_template.html')
+
+    
