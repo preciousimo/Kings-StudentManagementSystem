c0a867dd5cfd588be0682b73ebe6755fbc5a90a8
Mary Solomon <johnsolo2002@gmail.com>
2022-02-13 22:29:06 +0100

    registration working but redirect invalid

--- student_management_system/accounts/views.py
+++ student_management_system/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.contrib.auth import login, logout, authenticate
+from student_management_app.models import CustomUser
 from django.contrib import messages
 from student_management_app import urls
 from student_management_app.EmailBackEnd import EmailBackEnd
@@ -10,22 +11,28 @@ def registerPage(request):
     if request.method == 'POST':
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
-        username = request.POST['username']
+        username = request.POST.get('username')
         email = request.POST['email']
         password = request.POST['password']
         password2 = request.POST['password2']
         if password == password2:
-            if User.objects.filter(username=username).exists():
+            '''
+            if EmailBackEnd.objects.filter(username=username).exists():
                 messages.info(request, '{} already taken'.format(username))
                 return render(request, 'register.html')
-            elif User.objects.filter(email=email).exists():
-                messages.info(request, '{} already taken'.format(username))
+            elif EmailBackEnd.objects.filter(email=email).exists():
+                messages.info(request, '{} already taken'.format(email))
                 return render(request, 'register.html')
             else:
-                user = User.objects.create_user(first_name=first_name,last_name=last_name,username=username,email=email,password=password)
+                '''
+            try:
+                user = CustomUser.objects.create_user(first_name=first_name,last_name=last_name,username=username,email=email,password=password, user_type=1)
                 user.save() 
                 messages.success(request, '{} created successfully'.format(username))   
-                return render(request,'login.html')  
+                return redirect('/admin-home')  
+            except:
+                messages.error(request,'Invalid Credentials') 
+                return render(request, 'register.html')
         else:
             messages.error(request,'Passwords do not match') 
             return render(request, 'register.html')
