fc99822776685f57c291cad6005a25a4779adc70
Mary Solomon <johnsolo2002@gmail.com>
2022-02-14 22:23:13 +0100

    Fetching requests from student_template in StudentViews

--- student_management_system/student_management_app/StudentViews.py
+++ student_management_system/student_management_app/StudentViews.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib import messages
-from django.http import HttpResponse
+from student_management_app.models import CustomUser
+
 from student_management_app.models import Students
 def studentHome(request):
     return render(request, 'student_templates/home_content.html')
@@ -10,6 +11,8 @@ def addStudent(request):
         first_name = request.POST['first_name']
         middle_name = request.POST['middle_name']
         last_name = request.POST['last_name']
+        username = request.POST['username']
+        password = request.POST['password']
         date_of_birth = request.POST['date_of_birth']
         email = request.POST['email']
         phone_number = request.POST['phone_number']
@@ -18,8 +21,24 @@ def addStudent(request):
         address = request.POST['address']
         state = request.POST['state']
         nationality = request.POST['nationality']
+        term_start_month = request.POST['term_start_month']
+        term_end_month = request.POST['term_end_month']
+        session_start_year = request.POST['session_start_year']
+        session_end_year = request.POST['session_end_year']
         try:
-            new_student = Students.objects.create(first_name=first_name,middle_name=middle_name,last_name=last_name,date_of_birth=date_of_birth,email=email,phone_number=phone_number,gender=gender,address=address,state=state,nationality=nationality)
+            new_student = CustomUser.objects.create_user(first_name=first_name,last_name=last_name,username=username,email=email,password=password, user_type=3)
+            new_student.students.middle_name = middle_name
+            new_student.students.date_of_birth = date_of_birth
+            new_student.students.email = email
+            new_student.students.phone_number = phone_number
+            new_student.students.gender = gender
+            new_student.students.address = address
+            new_student.students.state = state
+            new_student.students.nationality = nationality
+            new_student.students.term_start_month = term_start_month
+            new_student.students.term_end_month = term_end_month
+            new_student.students.session_start_year = session_start_year
+            new_student.students.session_end_year = session_end_year
             new_student.save()
             messages.success(request, '{} added successfully'.format(first_name))
             return render(request, 'student_templates/add_student_template.html')   
