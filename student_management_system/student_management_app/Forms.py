f3cac404d158762a62329d5c90b931f6241fc534
Mary Solomon <johnsolo2002@gmail.com>
2022-02-22 18:52:30 +0100

    modified django forms layout

--- student_management_system/student_management_app/Forms.py
+++ student_management_system/student_management_app/Forms.py
@@ -1,23 +1,23 @@
 from django import forms
-
+from django.forms.widgets import TextInput, FileInput
 class DateInput(forms.DateInput):
     input_type = "date"
 
 class AddStudentForm(forms.Form):
-    first_name = forms.CharField(label = "First Name",max_length=50)
-    middle_name = forms.CharField(label = "Middle Name",max_length=50)
-    last_name = forms.CharField(label = "Last Name",max_length=50)
-    username = forms.CharField(label = "Username",max_length=50)
-    email = forms.EmailField(label = "Email",max_length=50, widget=forms.EmailInput())
-    password = forms.CharField(label = "Password",max_length=50, widget=forms.PasswordInput())
-    date_of_birth = forms.DateField(widget=DateInput())
-    phone_number = forms.CharField(label = "Phone Number",max_length=50)
-    gender = forms.CharField(label = "Gender",max_length=50)
-    profile_picture = forms.FileField(label = "Profile Picture",max_length=50)
-    address = forms.CharField(label = "Address",max_length=50)
-    state = forms.CharField(label = "State",max_length=50)
-    nationality = forms.CharField(label = "Nationality",max_length=50)
-    term_start = forms.DateField(label = "Term Begins", widget=DateInput())
-    term_end = forms.DateField(label = "Term Ends", widget=DateInput())
-    session_start = forms.DateField(label = "Session Begins", widget=DateInput())
-    session_end = forms.DateField(label = "Session Ends", widget=DateInput())
+    first_name = forms.CharField(label = "First Name",max_length=50, widget=TextInput(attrs={'class':'form-control'}))
+    middle_name = forms.CharField(label = "Middle Name",max_length=50, widget=TextInput(attrs={'class':'form-control'}))
+    last_name = forms.CharField(label = "Last Name",max_length=50, widget=TextInput(attrs={'class':'form-control'}))
+    username = forms.CharField(label = "Username",max_length=50, widget=TextInput(attrs={'class':'form-control'}))
+    email = forms.EmailField(label = "Email",max_length=50, widget=forms.EmailInput(attrs={'class':'form-control'}))
+    password = forms.CharField(label = "Password",max_length=50, widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    date_of_birth = forms.DateField(widget=DateInput(attrs={'class':'form-control'}))
+    phone_number = forms.CharField(label = "Phone Number",max_length=50, widget=TextInput(attrs={'class':'form-control'}))
+    gender = forms.CharField(label = "Gender",max_length=50, widget=TextInput(attrs={'class':'form-control'}))
+    profile_picture = forms.FileField(label = "Profile Picture",max_length=50, widget=FileInput(attrs={'class':'form-control'}))
+    address = forms.CharField(label = "Address",max_length=50, widget=TextInput(attrs={'class':'form-control'}))
+    state = forms.CharField(label = "State",max_length=50, widget=TextInput(attrs={'class':'form-control'}))
+    nationality = forms.CharField(label = "Nationality", max_length=50, widget=TextInput(attrs={'class':'form-control'}))
+    term_start = forms.DateField(label = "Term Begins", widget=DateInput(attrs={'class':'form-control'}))
+    term_end = forms.DateField(label = "Term Ends", widget=DateInput(attrs={'class':'form-control'}))
+    session_start = forms.DateField(label = "Session Begins", widget=DateInput(attrs={'class':'form-control'}))
+    session_end = forms.DateField(label = "Session Ends", widget=DateInput(attrs={'class':'form-control'}))
