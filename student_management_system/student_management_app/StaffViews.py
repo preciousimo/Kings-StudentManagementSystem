38a7cd007716c9c4917d25dcce3aa9e27d9683ae
Mary Solomon <johnsolo2002@gmail.com>
2022-02-23 21:55:00 +0100

    pushing data into db with django forms using forms.cleaned_data

--- student_management_system/student_management_app/StaffViews.py
+++ student_management_system/student_management_app/StaffViews.py
@@ -13,39 +13,43 @@ def addStaff(request):
 
 def addStaffSave(request):
     if request.method == 'POST':
-        first_name = request.POST['first_name']
-        middle_name = request.POST['middle_name']
-        last_name = request.POST['last_name']
-        username = request.POST['username']
-        password = request.POST['password']
-        date_of_birth = request.POST['date_of_birth']
-        email = request.POST['email']
-        phone_number = request.POST['phone_number']
-        gender = request.POST['gender']
-        address = request.POST['address']
-        state = request.POST['state']
-        nationality = request.POST['nationality']
-        profile_picture = request.FILES['profile_picture']
-        fs = FileSystemStorage()
-        filename = fs.save(profile_picture.name,profile_picture)
-        profile_picture_url = fs.url(filename)
-
-        try:
-            new_staff = CustomUser.objects.create_user(first_name=first_name,last_name=last_name,username=username,password=password,email=email, user_type=2)
-            new_staff.staffs.middle_name = middle_name
-            new_staff.staffs.date_of_birth = date_of_birth
-            new_staff.staffs.phone_number = phone_number
-            new_staff.staffs.gender = gender
-            new_staff.staffs.profile_picture = profile_picture_url
-            new_staff.staffs.address = address
-            new_staff.staffs.state = state
-            new_staff.staffs.nationality = nationality
-            new_staff.save()
-            messages.success(request, '{} created successfully'.format(username))
-            return redirect('/add-staff')   
-        except:
-            messages.error(request, 'Failed to add new staff')
-            return redirect('/add-staff')   
+        form = AddStaffForm(request.POST, request.FILES)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            middle_name = form.cleaned_data['middle_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            date_of_birth = form.cleaned_data['date_of_birth']
+            email = form.cleaned_data['email']
+            phone_number = form.cleaned_data['phone_number']
+            gender = form.cleaned_data['gender']
+            address = form.cleaned_data['address']
+            state = form.cleaned_data['state']
+            nationality = form.cleaned_data['nationality']
+            profile_picture = request.FILES['profile_picture']
+            fs = FileSystemStorage()
+            filename = fs.save(profile_picture.name,profile_picture)
+            profile_picture_url = fs.url(filename)
+            try:
+                new_staff = CustomUser.objects.create_user(first_name=first_name,last_name=last_name,username=username,password=password,email=email, user_type=2)
+                new_staff.staffs.middle_name = middle_name
+                new_staff.staffs.date_of_birth = date_of_birth
+                new_staff.staffs.phone_number = phone_number
+                new_staff.staffs.gender = gender
+                new_staff.staffs.profile_picture = profile_picture_url
+                new_staff.staffs.address = address
+                new_staff.staffs.state = state
+                new_staff.staffs.nationality = nationality
+                new_staff.save()
+                messages.success(request, '{} created successfully'.format(username))
+                return redirect('/add-staff')   
+            except:
+                messages.error(request, 'Failed to add new staff')
+                return redirect('/add-staff')   
+        else:
+            form = AddStaffForm(request.POST)
+            return render(request, 'staff_templates/add_staff_template.html', {'form':form})    
     else:
         return render(request, 'staff_templates/add_staff_template.html')
 
@@ -59,30 +63,30 @@ def editStaff(request, staff_id):
 
 def editStaffSave(request):
     if request.method == 'POST':
-        staff_id = request.POST['staff_id']
-        first_name = request.POST['first_name']
-        middle_name = request.POST['middle_name']
-        last_name = request.POST['last_name']
-        username = request.POST['username']
-        date_of_birth = request.POST['date_of_birth']
-        email = request.POST['email']
-        phone_number = request.POST['phone_number']
-        gender = request.POST['gender']
+        staff_id = form.cleaned_data['staff_id']
+        first_name = form.cleaned_data['first_name']
+        middle_name = form.cleaned_data['middle_name']
+        last_name = form.cleaned_data['last_name']
+        username = form.cleaned_data['username']
+        date_of_birth = form.cleaned_data['date_of_birth']
+        email = form.cleaned_data['email']
+        phone_number = form.cleaned_data['phone_number']
+        gender = form.cleaned_data['gender']
         if request.FILES['profile_picture']:
             profile_picture = request.FILES.get('profile_picture',False)
             fs = FileSystemStorage()
             filename = fs.save(profile_picture.name,profile_picture)
             profile_picture_url = fs.url(filename)
-        profile_picture = request.POST['profile_picture']
+        profile_picture = form.cleaned_data['profile_picture']
         if request.FILES.get('curicculum_vitae', False):
             curicculum_vitae = request.FILES['curicculum_vitae']
             fs = FileSystemStorage()
             filename = fs.save(curicculum_vitae.name,curicculum_vitae)
             curicculum_vitae_url = fs.url(filename)
-        curicculum_vitae = request.POST['curicculum_vitae']
-        address = request.POST['address']
-        state = request.POST['state']
-        nationality = request.POST['nationality']
+        curicculum_vitae = form.cleaned_data['curicculum_vitae']
+        address = form.cleaned_data['address']
+        state = form.cleaned_data['state']
+        nationality = form.cleaned_data['nationality']
         try:
             user = CustomUser.objects.get(id=staff_id)
             user.first_name = first_name
@@ -115,11 +119,11 @@ def editStaffSave(request):
 
 def addSubject(request):
     if request.method == 'POST':
-        subject_name = request.POST['subject_name']
-        subject_status = request.POST['subject_status']
+        subject_name = form.cleaned_data['subject_name']
+        subject_status = form.cleaned_data['subject_status']
         staff_id = request.POST.get('staff')
         staff = Staffs.objects.get(id)
-        admin_id = request.POST['admin']
+        admin_id = form.cleaned_data['admin']
         admin = SchoolAdmin.objects.get(id=admin_id)
         
         try:
