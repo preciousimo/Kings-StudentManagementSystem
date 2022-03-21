65f09fc25659331c51dcc0b0c71e19b6e6cb5cf6
Mary Solomon <jolomonson@gmail.com>
2022-03-21 12:25:49 +0100

    smtp auth error
    Signed-off-by: Mary Solomon <jolomonson@gmail.com>

--- student_management_system/student_management_system/settings.py
+++ student_management_system/student_management_system/settings.py
@@ -144,4 +144,4 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = "KIS Student Management System"
+DEFAULT_FROM_EMAIL = "KIS Student Management System <>"
