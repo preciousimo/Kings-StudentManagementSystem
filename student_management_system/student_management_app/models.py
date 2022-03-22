7fb8c35fcae99b03028d8f47946ca70d8c2ef74b
Mary Solomon <jolomonson@gmail.com>
2022-03-22 21:50:33 +0100

    Created LeaveReportStudent
    Signed-off-by: Mary Solomon <jolomonson@gmail.com>

--- student_management_system/student_management_app/models.py
+++ student_management_system/student_management_app/models.py
@@ -88,6 +88,17 @@ class AttendanceReport(models.Model):
     updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
 
+class LeaveReportStudent(models.Model):
+    id = models.AutoField(primary_key=True)
+    student_id = models.ForeignKey(Students, on_delete=models.CASCADE)
+    leave_date = models.CharField(max_length=255)
+    return_date = models.CharField(max_length=255)
+    leave_message = models.TextField()
+    leave_status = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
 class LeaveReportStaff(models.Model):
     id = models.AutoField(primary_key=True)
     staff_id = models.ForeignKey(Staffs, on_delete=models.CASCADE)
