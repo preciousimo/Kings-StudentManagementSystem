405fd42a8d4600a0d56d3e11283e191455982f43
Mary Solomon <jolomonson@gmail.com>
2022-04-02 12:59:18 +0100

    Deploying kis_sms to heroku
    Signed-off-by: Mary Solomon <jolomonson@gmail.com>

--- student_management_system/student_management_system/settings.py
+++ student_management_system/student_management_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'geufe!cc4m-%nrss09kw+hn3w1-fj98e-5via#-d)o&le*cf29'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kisstudentmanagementsystem.herokuapp.com/']
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
